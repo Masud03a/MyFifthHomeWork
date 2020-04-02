ab7f853f03315efca6a99bd3f443e04adcf72ed9
unknown <amirkhonov.masud90@gmail.com>
2020-04-02 15:36:20 +0300

    dsa

---
+++ Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 3...");
+            float P = float.Parse(Console.ReadLine().ToString().Replace(".", ","));
+            float m = 1000.0f;
+            int counter = 0;
+            float i = 0;
+            if(P > 0 && P < 25)
+            {
+                for(i = m; i <= 1100.0f; i+=i*P/100)
+                {
+                    counter++;
+                }
+                
+                Console.WriteLine($"Кол-во месяцев: {counter}, итоговый размер вклада {i}");
+            }
+        }
+    }
+}
